19b9e919f799ccadf054f90851e43b99e7d9275c
Alan Bendjuya <alanbendjuya@Alans-MacBook-Pro.local>
2024-07-22 19:23:25 -0300

    Update website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/static";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
-  })
+      enabled: true
+    }
+  }),
+  integrations: [icon()]
 });
